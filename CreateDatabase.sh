f722b20f1d51236fcf2742d89b0c98404a07173e
Jakub Mikusek <j.mikusek01@gmail.com>
2024-05-22 20:58:13 +0200

    repaired check if database exist inside dockerfile

--- CreateDatabase.sh
+++ CreateDatabase.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-sleep 10
+sleep 20
 
 echo "Running configuration script..."
 
-echo "MalopolskaMap database id: "
-/opt/mssql-tools/bin/sqlcmd -I -S localhost -U SA -P $MSSQL_SA_PASSWORD -d master -Q "PRINT DB_ID('MalopolskaMap');"
+db_check=$(/opt/mssql-tools/bin/sqlcmd -I -S localhost -U SA -P $MSSQL_SA_PASSWORD -d master -Q "IF DB_ID('MalopolskaMap') IS NULL PRINT '0' ELSE PRINT '1';" | tr -d '\r' | tail -n 1)
 
-if [ $(/opt/mssql-tools/bin/sqlcmd -I -S localhost -U SA -P $MSSQL_SA_PASSWORD -d master -Q "PRINT DB_ID('MalopolskaMap');")=" \n" ]; then
+if [ "$db_check" = "1" ]; then
+	echo "Skipping. Database already exists."
+else
+	echo "MalopolskaMap database does not exist."
 
 	/opt/mssql-tools/bin/sqlcmd -I -S localhost -U SA -P $MSSQL_SA_PASSWORD -d master -i /MalopolskaMap_TSQL/CreateDatabase.sql
 	/opt/mssql-tools/bin/sqlcmd -I -S localhost -U SA -P $MSSQL_SA_PASSWORD -d master -i /MalopolskaMap_TSQL/CreateTables.sql
 	/opt/mssql-tools/bin/sqlcmd -I -S localhost -U SA -P $MSSQL_SA_PASSWORD -d master -i /MalopolskaMap_TSQL/Functions.sql
 	/opt/mssql-tools/bin/sqlcmd -I -S localhost -U SA -P $MSSQL_SA_PASSWORD -d master -i /MalopolskaMap_TSQL/LoadMaps.sql
-else
-	echo "Skipping. Database already exists."
 fi
 
 echo "Configuration complete."
